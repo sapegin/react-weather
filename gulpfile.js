21d13562cba0ad730528f7a03f5c00172086f1ac
Artem Sapegin <artem@sapegin.ru>
2015-03-26 11:51:49 +0300

    Improve debugging workflow.
    1. gulp-webpack → webpack.
    2. Remove gulp-if.
    3. Show Webpack and Stylus errors in terminal and with node-notify.
    4. Exit on error in non-watch mode.

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,67 @@
 var gulp = require('gulp');
-var gulpIf = require('gulp-if');
-var webpack = require('gulp-webpack');
+var gutil = require('gulp-util');
+var plumber = require('gulp-plumber');
 var stylus = require('gulp-stylus');
 var copy = require('gulp-copy');
 var ghPages = require('gulp-gh-pages');
+var webpack = require('webpack');
+var notifier = require('node-notifier');
 var browserSync = require('browser-sync');
 var stylobuild = require('stylobuild');
-var reload = browserSync.reload;
 
 var isDevelopment = process.env.NODE_ENV !== 'production';
+var isWatch = false;
+
+function reload() {
+	if (isDevelopment && isWatch) {
+		return browserSync.reload.apply(browserSync, arguments);
+	}
+	else {
+		return gutil.noop();
+	}
+}
+
+function notifyError(plugin, message, filepath, filename) {
+	console.log(gutil.colors.red.bold('\nError in ' + (filename || filepath) + '\n'));
+	console.log(message);
+	if (isWatch) {
+		notifier.notify({
+			title: '\uD83D\uDEA8 ' + plugin + ' error',
+			message: gutil.colors.stripColor(message).replace(/ +/g, ' '),
+			open: 'file://' + filepath
+		});
+	}
+	else {
+		process.exit(1);
+	}
+}
 
 gulp.task('webpack', function() {
-	return gulp.src('app/app.js')
-		.pipe(webpack(require('./webpack.config.js')))
-		.pipe(gulp.dest('build/'))
-		.pipe(gulpIf(isDevelopment, reload({stream: true})));
+	webpack(require('./webpack.config.js'), function(err, stats) {
+		var error, message;
+		if (stats.hasErrors()) {
+			error = stats.compilation.errors[0];
+			message = error.error;
+		}
+		else if (stats.hasWarnings()) {
+			error = stats.compilation.warnings[0];
+			message = error.warning;
+		}
+		if (error) {
+			notifyError('Webpack', message.toString(), error.module.resource, error.module.rawRequest);
+		}
+		else {
+			reload();
+		}
+	});
 });
 
 gulp.task('styles', function() {
 	return gulp.src('styles/index.styl')
+		.pipe(plumber({errorHandler: function(error) {
+			notifyError('Stylus', error.message.replace(/.*\n/, ''), error.filename);
+			this.emit('end');
+		}}))
 		.pipe(stylus({
 			url: {
 				name: 'embedurl'
@@ -40,10 +83,11 @@ gulp.task('styles', function() {
 			]
 		}))
 	.pipe(gulp.dest('build/'))
-	.pipe(gulpIf(isDevelopment, reload({stream: true})));
+	.pipe(reload({stream: true}));
 });
 
 gulp.task('watch', function() {
+	isWatch = true;
 	if (isDevelopment) {
 		browserSync({
 			notify: false,
